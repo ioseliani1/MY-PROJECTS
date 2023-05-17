9fd4dd6b07b51bfa9d2e34e91b71e86548502654
ioseliani1 <81998936+ioseliani1@users.noreply.github.com>
2023-05-17 22:45:15 +0400

    Add files via upload

---
+++ HOMEWORKJAVASCRIPT6/index.js
@@ -0,0 +1,29 @@
+const currentDate = new Date();
+
+switch (currentDate.getDay()){
+    case 0:
+        console.log("REST DAY")
+        break;
+    case 1:
+        console.log("WORK DAY")
+        break;
+    case 2:
+        console.log("Tuesday, Thursday and Friday Reeducate days ")
+        break;
+    case 3:
+        console.log("WORK DAY")
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        console.log("WORK DAY")
+        break;
+
+}
+
+
+
